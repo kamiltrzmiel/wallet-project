6662753ee8ea085941b7d2db229a8a405fc8490c
Kamila Adamska <121kama121@gmail.com>
2023-10-04 23:07:52 +0200

    add jsx for Currency

---
+++ src/components/Currency/Currency.jsx
@@ -0,0 +1,26 @@
+export const Currency = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Currency</th>
+          <th>Purchase</th>
+          <th>Sale</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>USD</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>EUR</td>
+          <td>0,5</td>
+          <td>0,4</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
