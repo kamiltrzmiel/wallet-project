afb29fe4a6d5ed98054ae552c3db89563eb9a610
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-10-05 11:17:48 +0200

    Add spinner components

---
+++ src/components/Loader/LoaderGlobal.jsx
@@ -0,0 +1,9 @@
+import { Loader, ContainerLoader } from './LoaderGlobal.styled';
+
+export const LoaderGlobal = () => {
+  return (
+    <ContainerLoader>
+      <Loader />
+    </ContainerLoader>
+  );
+};
