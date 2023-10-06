f7b2fb6e7ff67d2a9c8fa0fa42f82628acdcfe8d
Szymon Oponczewski <szymon_o@mac.com>
2023-10-06 20:21:14 +0200

    Add inputs with styling for Table

---
+++ src/components/Inputs/BaseInput.styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const BaseInput = styled.input`
+  border: none;
+  border-bottom: 1px solid var(--color-switch-main);
+  font-family: Circe;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.33;
+  color: var(--font-color-dark);
+  max-width: 100%;
+  margin-bottom: 10px;
+  width: 280px;
+
+  &::placeholder {
+    color: var(--color-logout-button);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus {
+    outline: none;
+
+    + svg {
+      fill: var(--color-brand-primary);
+      transition: fill 150ms;
+    }
+  }
+
+  + svg {
+    width: 24px;
+    height: 24px;
+    `;
---
+++ src/components/Inputs/PasswordStrength.jsx
@@ -0,0 +1,37 @@
+import { passwordStrength } from 'check-password-strength';
+import { StrengthBar } from './PasswordStrength.styled';
+
+const PasswordStrength = props => {
+  const strength = passwordStrength(props.value, [
+    {
+      id: 0,
+      value: 'Tooweak',
+      minDiversity: 0,
+      minLength: 0,
+    },
+    {
+      id: 1,
+      value: 'Weak',
+      minDiversity: 2,
+      minLength: 6,
+    },
+    {
+      id: 2,
+      value: 'Medium',
+      minDiversity: 3,
+      minLength: 8,
+    },
+    {
+      id: 3,
+      value: 'Strong',
+      minDiversity: 4,
+      minLength: 12,
+    },
+  ]).value;
+
+  let progress;
+
+  if (strength === 'Tooweak') {
+    progress = 25;
+  }
+};
