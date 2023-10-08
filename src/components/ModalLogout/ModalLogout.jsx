847807a1c03ffbc0fc1d43899224123888265786
Kamila Adamska <121kama121@gmail.com>
2023-10-08 13:18:53 +0200

    add reset redux state when logout

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -4,16 +4,34 @@ import {
   YesBtn,
   NoBtn,
 } from './ModalLogout.styled';
+import { resetCurrency } from 'redux/currency/currencySlice';
+import { resetTransactions } from 'redux/slices/financeSlice';
+import { resetGlobal } from 'redux/slices/globalSlice';
+import { resetSession } from 'redux/slices/sessionSlice';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const ModalLogout = () => {
   const bodyEl = document.querySelector('body');
   bodyEl.style.overflow = 'hidden';
 
+  const dispatch = useDispatch();
+  const resetState = () => {
+    dispatch(resetCurrency());
+    dispatch(resetTransactions());
+    dispatch(resetGlobal());
+    dispatch(resetSession());
+  };
+
   return (
     <ModalContainer>
       <ModalWindow>
         <p>Are you sure you want to log out?</p>
-        <YesBtn type="button">Yes</YesBtn>
+        <Link to="/login">
+          <YesBtn type="button" onClick={resetState}>
+            Yes
+          </YesBtn>
+        </Link>
         <NoBtn type="button">No</NoBtn>
       </ModalWindow>
     </ModalContainer>
