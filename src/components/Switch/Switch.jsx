ea6d0779da47576f4a7adedda23575aac303c0d6
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-10-08 12:55:54 +0200

    Add Switch

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,26 @@
+import { Icon } from 'components/Icon/Icon';
+import { useField } from 'formik';
+import {
+  StyledSwitch,
+  SwitchCheckbox,
+  SwitchSlider,
+  SwitchText,
+} from './Switch.styled';
+
+const Switch = ({ ...props }) => {
+  const [field] = useField(props);
+
+  return (
+    <StyledSwitch>
+      <SwitchText $isChecked={props.checked}>Income</SwitchText>
+      <SwitchSlider $isChecked={props.checked}>
+        <SwitchCheckbox {...field} {...props} />
+        <Icon icon="icon__btn-plus" $isChecked={props.checked} />
+        <Icon icon="icon__btn-minus" $isChecked={props.checked} />
+      </SwitchSlider>
+      <SwitchText $isChecked={props.checked}>Expanse</SwitchText>
+    </StyledSwitch>
+  );
+};
+
+export default Switch;
