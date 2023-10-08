497363d6f2c1a7d0832ae7b772882917d5c02c39
kamiltrzmiel <k.trzmiel@gmail.com>
2023-10-08 21:26:12 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import currencyReducer from './currency/currencySlice';
 import globalReducer from './slices/globalSlice';
 import sessionReducer from './slices/sessionSlice';
-import transactionsReducer from './slices/financeSlice';
+import financeReducer from './slices/financeSlice';
 
 const store = configureStore({
   reducer: {
     currency: currencyReducer,
     global: globalReducer,
     session: sessionReducer,
-    transactions: transactionsReducer,
+    finance: financeReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
 });
