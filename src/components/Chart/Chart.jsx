24a2778b190f6f1bce9bffef61607c05ac5b27a6
Kamila Adamska <121kama121@gmail.com>
2023-10-16 01:47:56 +0200

    fix chart and balance
    Naprawa wyświetlania - jak w danym miesiącu nie ma transakcji, to nic nie wyświetla, po załadowaniu statystyk wyświetla cały rok, a po wybraniu miesiąca zawierającego transakcje, wyświetla je poprawnie. Oblicza również balans w zależności od ww.

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,23 +4,61 @@ import { Doughnut } from 'react-chartjs-2';
 import { formatBalance } from 'utilities/numberUtils';
 import { ChartContainer, Balance } from './Chart.styled';
 import { getRandomColor } from 'utilities/getRandomColor';
+import {
+  sumAmountByCategory,
+  sumMonthlyIncomeAndExpenses,
+} from 'utilities/sumMonthlyTotals';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Chart = () => {
-  const { totals, monthlyTotals } = useSelector(state => state.finance);
-  const balance = useSelector(state => state.finance.totals.balance);
+  const { totals, monthlyTotals, selectedMonth, selectedYear } = useSelector(
+    state => state.finance
+  );
+  const totalBalance = useSelector(state => state.finance.totals.balance);
 
-  const showTotals = monthlyTotals && monthlyTotals.transactions;
+  const monthlyIncomeAndExpenses =
+    Object.keys(monthlyTotals).length === 0
+      ? null
+      : sumMonthlyIncomeAndExpenses(monthlyTotals);
+
+  const monthlyTotalsByCategory =
+    Object.keys(monthlyTotals).length === 0
+      ? null
+      : sumAmountByCategory(monthlyTotals);
+
+  const showTotals =
+    selectedMonth && selectedYear && monthlyTotals && monthlyTotalsByCategory;
+
+  let balance = 0;
+
+  if (monthlyIncomeAndExpenses) {
+    balance =
+      monthlyIncomeAndExpenses.monthlyIncome -
+      monthlyIncomeAndExpenses.monthlyExpenses;
+  } else if (!showTotals && !selectedMonth) {
+    balance = totalBalance;
+  }
+
   const dataToMap = showTotals
-    ? monthlyTotals.transactions
+    ? monthlyTotalsByCategory
     : totals.totalExpensesByCategories;
 
-  if (!dataToMap || totals.totalExpensesByCategories.length === 0) {
+  console.log('datki do mapki', dataToMap);
+
+  if (
+    !dataToMap ||
+    totals.totalExpensesByCategories.length === 0 ||
+    (Object.keys(monthlyTotals).length === 0 && selectedYear)
+  ) {
     return null;
   }
+
   const labels = dataToMap.map(item => item.category);
   const dataValues = dataToMap.map(item => item.amount);
+
+  console.log('labelki', labels);
+  console.log('wartości', dataValues);
 
   const backgroundColors = [];
   const numberOfColorsToGenerate = labels.length;
